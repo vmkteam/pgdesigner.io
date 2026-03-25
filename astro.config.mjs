7b5317fbe773e795a01d76eee9a8ec88958039e7
Sergey Bykov <sergeyfast@gmail.com>
2026-03-25 14:20:35 +0100

    Fix trailing slash in sitemap, add favicons, update download page

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
   site: "https://pgdesigner.io",
+  trailingSlash: "never",
   integrations: [mdx(), sitemap(), icon()],
   vite: {
     plugins: [tailwindcss()],
